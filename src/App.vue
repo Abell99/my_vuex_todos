33c99687466fc0c6c1f076036b7ed9de9ebe20a9
Abel <qq1104031080@outlook.com>
2020-05-07 18:49:10 +0800

    todos by vuex
    基于vuex实现的todes小案例

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,111 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <a-input :value="inputValue" @change="handleInputChange" placeholder="请输入任务" class="my_ipt" />
+    <a-button type="primary" @click="addItemToList">添加事项</a-button>
+    <a-list bordered :dataSource="infolist" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox @change="handleCheckboxChange(item.id)" :checked="item.done">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeItemById(item.id)">删除</a>
+      </a-list-item>
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDoneLength}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="viewKey === 'all' ? 'primary': ''" @click="changList('all')">全部</a-button>
+          <a-button :type="viewKey === 'undone' ? 'primary': ''" @click="changList('undone')">未完成</a-button>
+          <a-button :type="viewKey === 'done' ? 'primary': ''" @click="changList('done')">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="clean">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
-<style>
+<script>
+// 按需导入所需要的函数方法
+import { mapState, mapActions, mapMutations, mapGetters } from 'vuex'
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  // 计算机属性,在这里用来映射state和getters
+  computed: {
+    // 映射state
+    ...mapState(['list', 'inputValue', 'viewKey']),
+    // 映射处理过的数据
+    ...mapGetters(['unDoneLength', 'infolist'])
+  },
+  methods: {
+    // 异步函数映射
+    ...mapActions(['getList']),
+    // 映射函数，用来改变数据源数据值
+    ...mapMutations(['setInputValue', 'addItem', 'removeItem', 'checkboxChange', 'cleanDone', 'changeListIndex']),
+
+    // 同步改变备注输入框的内容
+    handleInputChange (e) {
+      // 通过e.target.value获取当前文本框的内容
+      this.setInputValue(e.target.value)
+    },
+    // 向列表中新增 item 项
+    addItemToList () {
+      // 判断文本框内容，如果文本框为空，则返回一条提示信息
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning('文本框不能为空')
+      }
+      // 如果文本框内容不为空，则调用mutations中的函数，从而来操作数据源内容
+      this.addItem()
+    },
+    // 根据Id值从列表中删除 item 项
+    removeItemById (Id) {
+      // 调用mutations函数来改变数据源数据
+      this.removeItem(Id)
+    },
+    // 选中状态的更改
+    handleCheckboxChange (Id) {
+      // 调用mutations函数来改变数据源数据
+      this.checkboxChange(Id)
+    },
+    // 清除已完成的任务
+    clean () {
+      this.cleanDone()
+    },
+    // 改变列表内容选项卡
+    changList (Index) {
+      this.changeListIndex(Index)
+    }
+  },
+  created () {
+    // 在加载页面的时候,发起请求,获取原始数据
+    this.getList()
+  }
+}
+</script>
+
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding: 10px;
 }
 
-#nav {
-  padding: 30px;
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,101 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 所有的任务列表
+    list: [],
+    // 文本框内容
+    inputValue: '',
+    // 新的数据结点，当前最大的Id值
+    nextId: 5,
+    // 控制列表内容显示的选项卡名称
+    viewKey: 'all'
   },
   mutations: {
+    // 赋值list的函数方法
+    initList (state, list) {
+      // console.log(list)
+      state.list = list
+    },
+    // 同步更改备注的文本框内容
+    setInputValue (state, value) {
+      // 将传入的实参value赋值给当前的文本框的内容
+      state.inputValue = value
+    },
+    // 用来添加item到list中去
+    addItem (state) {
+      // 初始化一个对象，因为每一条信息都是对象形式
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      // 将新声明的对象，拼接到list中，并渲染到页面中去
+      state.list.push(obj)
+      // 下一个id值进行累加，保证id值不会重复
+      state.nextId++
+      // 将当前的文本框内容置空，方便下一条数据的输入
+      state.inputValue = ''
+    },
+    // 根据Id值来删除list中的一项
+    removeItem (state, Id) {
+      // 通过findIndex来查询数组对象的对应索引值
+      const i = state.list.findIndex(x => x.id === Id)
+      // 根据索引值来删除对应的itme项
+      state.list.splice(i, 1)
+    },
+    // 改变item的选中状态
+    checkboxChange (state, Id) {
+      // 通过findIndex来查询数组对象的对应索引值
+      const i = state.list.findIndex(x => x.id === Id)
+      // 根据索引值，设置当前值取反
+      state.list[i].done = !state.list[i].done
+    },
+    // 清除已完成的任务
+    cleanDone (state) {
+      // 过滤done为false的
+      state.list = state.list.filter(x => x.done === false)
+    },
+    // 变更列表内容选项卡
+    changeListIndex (state, Index) {
+      state.viewKey = Index
+    }
   },
   actions: {
+    // 定义一个异步的函数方法
+    getList (context) {
+      // axios异步请求json文件
+      axios.get('/list.json')
+        // 在请求返回成功之后，返回data
+        .then(({ data }) => {
+          // 通过形参调用commit函数，调用mutations中的方法，间接的控制state中的数值的变化
+          context.commit('initList', data)
+        })
+    }
+  },
+  getters: {
+    unDoneLength (state) {
+    // 使用过滤器统计未完成的条数
+      return state.list.filter(x => x.done === false).length
+    },
+    // 根据当前的选项卡选项，来按需切换显示内容
+    infolist (state) {
+      // 由viewKey来决定显示的内容
+      if (state.viewKey === 'all') {
+        return state.list
+      } else if (state.viewKey === 'undone') {
+        // 通过过滤器来过滤数据
+        return state.list.filter(x => !x.done) // 过滤器根据当前内容是否为true判断是否通过
+      } else if (state.viewKey === 'done') {
+        return state.list.filter(x => x.done)
+      } else {
+        return state.list
+      }
+    }
   },
   modules: {
   }
